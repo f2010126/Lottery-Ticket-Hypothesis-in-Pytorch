02d114f2a4cbcce4bedde1dc873133ce02897fb5
Dipti Sengupta <diptisengupa@Diptis-MBP-2.fritz.box>
2022-02-06 12:13:43 +0100

    123 Seed Scripts
    Run scripts. W and W/o pretrained model. 123 Seed

---
+++ Cluster_Scripts/lth_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p mlhiwidlc_gpu-rtx2080
+#SBATCH -q dlc-dsengupt
+#SBATCH --gres=gpu:1
+#SBATCH -o /work/dlclarge1/dsengupt-lth_ws/slurm_logs/resnet_base_123.out # STDOUT  (the folder log has to be created prior to running or this won't work)
+#SBATCH -e /work/dlclarge1/dsengupt-lth_ws/slurm_logs/resnet_base_123.err # STDERR  (the folder log has to be created prior to running or this won't work)
+#SBATCH -J BaseLine_LTH_123
+#SBATCH -t 19:59:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+cd $(ws_find lth_ws)
+#python3 -m venv lth_env
+source lth_env/bin/activate
+
+pip list
+python3 -c "import torch; print(torch.__version__)"
+python3 -c "import torch; print(torch.cuda.is_available())"
+
+cd OtherLTHFrameworks/Lottery-Ticket-Hypothesis-in-Pytorch
+python3 -m main --prune_type=lt --arch_type=simsiam_resnet18 --trial LTH_BaseLine_123 --dataset=cifar10 --prune_percent=20 --lr 0.01 --batch_size 512 --seed 123 --prune_iterations=10 --end_iter=35
+deactivate
